862f32de466d5ee9e3b9061a0863ac64ab33743a
fleckm1 <fleck.missy@gmail.com>
2026-09-13 13:32:32 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
-const CACHE="road180-v2";const ASSETS=["./","./index.html","./manifest.json","./road180-background.png"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))])));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
+const CACHE="road180-v3";
+const ASSETS=["./","./index.html","./manifest.json","./road180-background.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener("activate",e=>e.waitUntil(Promise.all([
+ self.clients.claim(),
+ caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+])));
+self.addEventListener("fetch",e=>{
+ e.respondWith(fetch(e.request).then(r=>{
+   const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
+ }).catch(()=>caches.match(e.request)));
+});
